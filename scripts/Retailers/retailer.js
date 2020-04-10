b429a42f9cda1314af813055fbeec047ee03f57c
ianpenn08 <inkmpenn@gmail.com>
2020-04-10 11:34:08 -0500

    added distributors

--- scripts/Retailers/retailer.js
+++ scripts/Retailers/retailer.js
@@ -1,10 +1,22 @@
-export const Retailer = (retailerObject) => {
+export const Retailer = (retailerObject, distributorObject) => {
     return `
-    <section class="retailer">
-    <h2> ${retailerObject.name}</h2>
-    <div>Address: ${retailerObject.address} </div>
-    <div>City: ${retailerObject.city} </div>
-    <div>State: ${retailerObject.state} </div>
-    </section>
+    <div class="retailer">
+        <section class="retailer__name">
+        <header>
+            <h2> ${retailerObject.name}</h2>
+        </header>
+            <div>Address: ${retailerObject.address} </div>
+            <div>City: ${retailerObject.city} </div>
+            <div>State: ${retailerObject.state} </div>
+        </section>
+        <section class="distributor__name">
+        <header>
+            <h2>Distributor: ${distributorObject.name}</h2>
+        </header>
+            <div>Address: ${distributorObject.address} </div>
+            <div>City: ${distributorObject.city} </div>
+            <div>State: ${distributorObject.state} </div>
+        </section>
+    </div>
     `
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,12 @@ import { FlowerList } from "./flowerList.js";
 import { getFlowers } from "./flowerProvider.js";
 import { getRetailers } from "./Retailers/retailerProvider.js";
 import { RetailerList } from "./Retailers/retailerList.js";
+import { getDistributors } from "../Distributors/distributorProvider.js";
 
 
 getFlowers()
 .then(FlowerList)
 
 getRetailers()
+.then(getDistributors)
 .then(RetailerList)
